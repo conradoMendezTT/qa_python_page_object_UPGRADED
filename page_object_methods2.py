f7726310d8a4a7e0e21080a18fac107dc47f76d5
conradoTT <conrado.mendez@tripleten-team.com>
2025-01-09 23:38:27 -0300

    page_object_methods2.py == DONE

--- page_object_methods2.py
+++ page_object_methods2.py
@@ -1,30 +1,31 @@
 from selenium.webdriver.common.by import By
 
-
 class RegistrationPageAround:
-    # The Email field locator
-    email_field = ...
-    # The Password field locator
-    password_field = ...
-    # The Sign-up button locator
-    registration_button = ...
+    # El localizador del campo Correo electrónico
+    email_field = (By.ID, 'email')
+    # El localizador del campo Contraseña
+    password_field = (By.ID, 'password')
+    # El localizador del botón Registrarse
+    registration_button = (By.CLASS_NAME, 'auth-form__button')
 
-    # The class constructor
+    # El constructor de clase
     def __init__(self, driver):
-        ...
+        self.driver = driver
 
-    # The method fills in the Email field
+    # El método rellena el campo Correo electrónico
     def set_email(self, email):
-        ...
+        self.driver.find_element(*self.email_field).send_keys(email)
 
-    # The method fills in the Password field
+    # El método rellena el campo Contraseña
     def set_password(self, password):
-        ...
+        self.driver.find_element(*self.password_field).send_keys(password)
 
-    # The method clicks on the Sign-up button
+    # El método hace clic en el botón Registrarse
     def click_registration_button(self):
-        ...
+        self.driver.find_element(*self.registration_button).click()
 
-    # The sign-up method — it combines the email, the password, and the click
-    def register(self, ...):
-        ...
+    # El método de registro combina el correo electrónico, la contraseña y el clic del botón
+    def register(self, email, password):
+        self.set_email(email)
+        self.set_password(password)
+        self.click_registration_button()
