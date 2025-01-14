93cce40e23c5ae1e4da557bf92169c2180ad1ac7
conradoTT <conrado.mendez@tripleten-team.com>
2025-01-14 15:08:05 -0300

    Ex.3 == Done

--- practice_self_cls/Exercise_3.py
+++ practice_self_cls/Exercise_3.py
@@ -5,3 +5,25 @@
 # Agrega métodos de clase set_config y show_config para modificar e imprimir estas configuraciones.
 # Modifica las configuraciones globalmente y muestra cómo afectan a diferentes instancias.
 
+
+
+class Config:
+    setting1 = 0
+    setting2 = 0
+
+    @classmethod
+    def set_config(cls, new_setting1_config_code, new_setting2_config_code):
+        cls.setting1 = new_setting1_config_code
+        cls.setting2 = new_setting2_config_code
+
+    @classmethod
+    def show_config(cls):
+        print(f"Currently configuration is {cls.setting1} and {cls.setting2}")
+
+
+
+configuration_1 = Config()
+configuration_1.show_config()
+
+configuration_1.set_config(12 , 13)
+configuration_1.show_config()
