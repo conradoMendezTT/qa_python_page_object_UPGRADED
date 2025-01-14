8b3259527c1d350d5a406da15e69802109ab135e
conradoTT <conrado.mendez@tripleten-team.com>
2025-01-14 16:23:22 -0300

    in process, need to fix algorithm

--- practice_self_cls/Exercise_4.py
+++ practice_self_cls/Exercise_4.py
@@ -5,3 +5,27 @@
 # Agrega métodos de instancia para add y subtract, que modifiquen history con la operación realizada.
 # Agrega un método de clase clear_history para reiniciar la historia.
 # Crea instancias, realiza operaciones, y muestra cómo history refleja las operaciones realizadas.
+
+
+class Calculator:
+    history = []
+
+    def add (self, number_to_add):
+        Calculator.history.append(number_to_add)
+        for i in Calculator.history:
+
+            print(f"Actual history values are {i}" )
+
+    def substract (self, number_to_remove):
+        history = Calculator.history
+
+        for i in history:
+            if i == number_to_remove:
+                history.remove(i)
+
+calc_1 = Calculator()
+
+calc_1.add(1)
+calc_1.add(4)
+calc_1.add(6)
+
