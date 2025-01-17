29e8b05418e08638131d6913c4fc44ef273dca87
conradoTT <conrado.mendez@tripleten-team.com>
2025-01-17 13:08:44 -0300

    Ex.4 == DONE

--- practice_self_cls/Exercise_4.py
+++ practice_self_cls/Exercise_4.py
@@ -11,21 +11,45 @@ class Calculator:
     history = []
 
     def add (self, number_to_add):
-        Calculator.history.append(number_to_add)
-        for i in Calculator.history:
+       # Calculator.history.append(number_to_add) # --> Accedo al atributo de otra manera
+        self.history.append(number_to_add)
+        #for i in Calculator.history:
+        print(self.history)
+           # print(f"Actual history values are {i}" )
 
-            print(f"Actual history values are {i}" )
-
-    def substract (self, number_to_remove):
+    def subtract (self, number_to_remove):
         history = Calculator.history
 
         for i in history:
             if i == number_to_remove:
                 history.remove(i)
 
-calc_1 = Calculator()
+        print(f"El nuevo history que tenemos es: {self.history}")
 
-calc_1.add(1)
-calc_1.add(4)
-calc_1.add(6)
+        print(history)
+    @classmethod
+    def clear_history (cls):
+        cls.history.clear()
+
+
+#Creating Instances = Objects
+calc_1= Calculator()
+calc_2 = Calculator()
+calc_3 = Calculator()
+
+#Adding numbers of instances / methods
+calc_1.add(12)
+calc_2.add(124)
+calc_3.add(1234)
+
+
+#Substracting numbers of instances / methods
+calc_1.subtract(12)
+
+#Cleaning Class attribute
+print("Borrando el history....")
+Calculator.clear_history()
+
+
+print(f"El history ahora es {Calculator.history}")
 
