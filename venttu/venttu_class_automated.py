6525e0a0b3e6c3f8c7a66de286a6083addb91788
conradoTT <conrado.mendez@tripleten-team.com>
2025-02-05 22:10:03 -0300

    details and new method for error message in password

--- venttu/venttu_class_automated.py
+++ venttu/venttu_class_automated.py
@@ -15,16 +15,21 @@ class LoginPageVenttu:
         self.driver = driver
 
     def send_email(self, email_to_send):
-         email_field = WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(self.email_field)) #La funcion recibe una tupla
+         email_field = WebDriverWait(self.driver, 10).until(expected_conditions
+                                                            .visibility_of_element_located(self.email_field)) #La funcion recibe una tupla
          email_field.send_keys(email_to_send)
+         email_field.clear()
+         rmai
 
     def send_password(self, password_to_send):
-        pass_field = WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(self.password_field))
+        pass_field = WebDriverWait(self.driver, 10).until(expected_conditions
+                                                          .visibility_of_element_located(self.password_field))
         pass_field.send_keys(password_to_send)
 
     def click_sign_in_button(self):
         log_in_btn = WebDriverWait(self.driver, 10).until(
-            expected_conditions.visibility_of_element_located(self.sign_in_button))
+            expected_conditions
+                .visibility_of_element_located(self.sign_in_button))
         log_in_btn.click()
 
     def login(self, email, password):
@@ -36,6 +41,15 @@ class LoginPageVenttu:
         try:
             email_warning = WebDriverWait(self.driver, 10).until(
                 expected_conditions.visibility_of_element_located(self.email_error_warning)
+            )
+            return email_warning.is_displayed()
+        except:
+            return False  # Si no se encuentra o no está visible, retorna False
+
+    def is_password_warning_message_displayed(self):
+        try:
+            email_warning = WebDriverWait(self.driver, 10).until(
+                expected_conditions.visibility_of_element_located(self.password_error_warning)
             )
             return email_warning.is_displayed()
         except:
@@ -59,7 +73,6 @@ class TestVenttu:
     def test_set_email(self):
         # Crea una clase de objeto de página para la página de inicio de sesión
         login_venttu = LoginPageVenttu(self.driver)
-        # Iniciar sesión
         login_venttu.login('correo@electrónico', 'contraseña')
 
 
@@ -70,11 +83,13 @@ class TestVenttu:
 
     def test_display_warning_password_box(self):
         login = LoginPageVenttu(self.driver)
+        login.send_email("tripleten_cohort_20@POC.com")
         login.click_sign_in_button()
-        assert login.is_email_warning_message_displayed() == True
+        assert login.is_password_warning_message_displayed() == True
 
 
     @classmethod
     def teardown_class(cls):
         #Cerrar el navegador
+        cls.driver.quit()
         cls.driver.quit()
